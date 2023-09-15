b3e7db525e89dd077650cd3983e5beeb096b8b93
marcoosdantas <2226973@aluno.univesp.br>
2023-09-14 23:55:17 -0300

    alteraçoes e inicio do projeto integrador

--- exception/Exercicio.java
+++ exception/Exercicio.java
@@ -5,60 +5,42 @@ import java.util.Scanner;
 public class Exercicio {
 
 	public static void main(String[] args) {
-		int opcao, quantidade;
+		int opcao;
+		float saldo = 1000, operacao;
 		Scanner leia = new Scanner(System.in);
-		System.out.println("Código de produto: ");
-		opcao = leia.nextInt();
-		switch (opcao) {
-		case 1:
-			System.out.println("Cachorro Quente");
-			break;
-		case 2:
-			System.out.println(
-                "X-Salada");
-			break;
-		case 3:
-			System.out.println("X-Bacon");
-			break;
-		case 4:
-			System.out.println("Bauru");
-			break;
-		case 5:
-			System.out.println(
-                "Refrigerante");
-			break;
-		case 6:
-			System.out.println("Suco de Laranja");
-			break;
-		default:
-			System.out.println("Opção inválida!");
-		}
-		
-		System.out.println("Quantidade ");
-		quantidade = leia.nextInt();
+		while (true) {
 
-		switch (opcao) {
-		case 1:
-			System.out.println("Valor total: R$ " + 10 * quantidade);
-			break;
-		case 2:
-			System.out.println(
-                "Valor total: R$ " + 15 * quantidade);
-			break;
-		case 3:
-			System.out.println("Valor total: R$ " + 18 * quantidade);
-			break;
-		case 4:
-			System.out.println("Valor total: R$ " + 12 * quantidade);
-			break;
-		case 5:
-			System.out.println("Valor total: R$ " + 8 * quantidade);
-			break;
-		case 6:
-			System.out.println("Valor total: R$ " + 13 * quantidade);
-			break;
-		default:
-			System.out.println("Opção inválida!");
+			System.out.println("# ## ### #### #########   Menu   ########## #### ### ## #");
+
+			System.out.println("\n--Digite 1 para visualizar o saldo--");
+			System.out.println("--Digite 2 para realizar um saque--");
+			System.out.println("--Digite 3 para realizar um depósito--");
+			opcao = leia.nextInt();
+
+			switch (opcao) {
+			case 1:
+				System.out.println("Saldo: " + saldo);
+				break;
+			case 2:
+				System.out.println("Digite o valor do saque: ");
+				operacao = leia.nextFloat();
+				if (operacao <= saldo) {
+					saldo -= operacao;
+				} else {
+					System.out.println("Saldo insuficiente!");
+				}
+				System.out.println("Saldo: " + saldo);
+				break;
+			case 3:
+				System.out.println("Digite o valor do depósito: ");
+				operacao = leia.nextFloat();
+				saldo += operacao;
+				System.out.println("Saldo: " + saldo);
+				break;
+			default:
+				throw new RuntimeException("Digite uma opção de 1 a 3.");
+			}
 		}
+
 	}
 }
